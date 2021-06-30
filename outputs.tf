ccadf235152fe5d7c5e4d4cd6f0ab254386cacc6
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-06-30 17:37:19 +0200

    Of course we need to output the credentials

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "service_id" {
   description = "The service id"
   value       = cloudfoundry_service_instance.postgres.id
 }
+
+output "credentials" {
+  description = "The service credentials"
+  sensitive = true
+  value = cloudfoundry_service_key.database_key.credentials
+}
